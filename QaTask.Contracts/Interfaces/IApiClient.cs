2804a3064fcaf5ecbbe4c5bb985d830a907b3d87
Ioannis Petsoglou <johnpets92@gmail.com>
2024-12-25 17:43:31 +0200

    API tests Implementation

---
+++ QaTask.Contracts/Interfaces/IApiClient.cs
@@ -0,0 +1,18 @@
+﻿using QaTask.Contracts.Models;
+
+namespace QaTask.Contracts.Interfaces;
+
+public interface IApiClient
+{
+    /// Fetch a user by username from the API.
+    Task<UserModel?> GetUser(string? username);
+
+    /// Create multiple users with a list of user data.
+    Task<GenericResponse?> CreateUsersWithListAsync(List<UserModel> users);
+
+    /// Update a user by username.
+    Task<GenericResponse?> UpdateUser(string? username, UserModel updatedUser);
+
+    /// Delete a user by username.
+    Task<GenericResponse?> DeleteUser(string username);
+}
